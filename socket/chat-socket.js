aa9e258a68cb3144175bba4d294d6b3e46ccbc1c
Nazmus Sayad <247sayad@gmail.com>
2022-07-23 01:06:32 +0600

    error fixed

--- socket/chat-socket.js
+++ socket/chat-socket.js
@@ -1,46 +1,66 @@
 const Chat = require("../model/Chat.js")
 const User = require("../model/User.js")
 
-module.exports = async function (socket) {
-  console.log(socket.handshake.query)
+const sendInitialMessages = async (respond) => {
+  try {
+    const data = await Chat.getLastMessages()
+    respond(data)
+  } catch (err) {
+    throw err
+  }
+}
 
+const getOlderMessagesThanId = async (id, respond) => {
+  try {
+    const data = await Chat.getOlderMessagesThanId(id)
+    respond(data)
+  } catch (err) {
+    throw err
+  }
+}
+
+const getNewerMessagesThanId = async (id, respond) => {
+  try {
+    const data = await Chat.getNewerMessagesThanId(id)
+    if (data.length > 100) return respond(data.length)
+    respond(data)
+  } catch (err) {
+    throw err
+  }
+}
+
+const writeNewMessage = async function (msgs, respond) {
+  try {
+    const data = await Chat.writeMessage(this.email, msgs)
+    socket.broadcast.emit("message-new", data)
+    respond(data)
+  } catch (err) {
+    throw err
+  }
+}
+
+const onDisconnect = () => {}
+
+module.exports = async function (socket) {
   try {
     const user = User.getMatchedUser(
       socket?.handshake?.auth?.email,
       socket?.handshake?.auth?.password
     )
 
-    socket.on("message-initial", async (respond) => {
-      const data = await Chat.getLastMessages()
-      respond(data)
-    })
+    console.log(`---> Connected with "${user.email}"`)
 
-    socket.on("message-getOlder", async (id, respond) => {
-      const data = await Chat.getOlderMessagesThanId(id)
-      respond(data)
-    })
-
-    socket.on("message-getNewer", async (id, respond) => {
-      const data = await Chat.getNewerMessagesThanId(id)
-      if (data.length > 100) return respond(data.length)
-      respond(data)
-    })
-
-    socket.on("message-new", async (msgs, respond) => {
-      const data = await Chat.writeMessage(user.email, msgs)
-      socket.broadcast.emit("message-new", data)
-      respond(data)
-    })
+    socket.on("message-initial", sendInitialMessages.bind(user))
+    socket.on("message-getOlder", getOlderMessagesThanId.bind(user))
+    socket.on("message-getNewer", getNewerMessagesThanId.bind(user))
+    socket.on("message-new", writeNewMessage.bind(user))
+    socket.on("disconnect", onDisconnect)
 
     /* 
     socket.on("message-edit", async (data, respond) => {})
     socket.on("message-delete", async (data, respond) => {})
     */
-    socket.on("disconnect", () => {
-      // console.log(socket)
-    })
   } catch (err) {
-    console.log(err)
     socket.emit("error", err.message)
     socket.disconnect()
   }
