ced9cc6478b2f0c8747ac1e839b615e34ba08b41
Nazmus Sayad <247sayad@gmail.com>
2022-08-14 19:29:44 +0600

    ping

--- router.js
+++ router.js
@@ -1,18 +1,20 @@
-const express = require("express")
+const express = require('express')
 
-const app = require("./app.js")
-const route = require("./routes/user-route.js")
-const User = require("./model/User.js")
-const HelloWorld = require("./hello-world")
-const { API_URL } = require("./.config.js")
+const app = require('./app.js')
+const route = require('./routes/user-route.js')
+const User = require('./model/User.js')
+const HelloWorld = require('./hello-world')
+const { API_URL } = require('./.config.js')
 
 const router = express.Router()
-router.route("/user/:id").get(route.getUserPublicInfo)
-router.route("/user").get(route.checkUser).post(route.createUser)
+router.route('/user/:id').get(route.getUserPublicInfo)
+router.route('/user').get(route.checkUser).post(route.createUser)
 
-app.use("/", async (req, res, next) => {
+app.use('/', async (req, res, next) => {
   if (!User.userLoaded) await User.waitForUserLoading()
   next()
 })
 app.use(API_URL, router)
-app.use("/", (req, res) => HelloWorld(res))
+
+app.use('/ping', (req, res) => res.end())
+app.use('/', (req, res) => HelloWorld(res))
