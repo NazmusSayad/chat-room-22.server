7bc65026ae442c5d0ca0449ceb15b7bc15a3f78d
Nazmus Sayad <247sayad@gmail.com>
2022-08-29 01:41:30 +0600

    db

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
-if (!process.env.DATABASE_URL) {
+if (!process.env.DB) {
   throw new Error("Where is DATABASE token?")
 }
 
 mongoose
-  .connect(process.env.DATABASE_URL)
+  .connect(process.env.DB)
   .then(() => console.log(">>> MongoDB connected successfully..."))
